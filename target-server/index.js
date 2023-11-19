7960e1faaffb2ac6350792884803328923c54e7b
Angel Almanza <almanza.angel245@gmail.com>
2023-11-19 15:37:26 -0700

    Code for the creation of the target server and attacker server is created. In this commit, only the base version exists without any security measures, and it includes a highly abusive and easily detectable attack

---
+++ target-server/index.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const session = require('express-session');
+const handlebars = require('express-handlebars');
+const fs = require('fs');
+
+const app = express();
+const PORT = 3000;
+
+// Middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  secret: 'test',
+  resave: false,
+  saveUninitialized: false,
+}));
+app.set("views", __dirname);
+app.engine("hbs", handlebars.engine({
+  defaultLayout: 'main',
+  layoutsDir: __dirname,
+  extname: '.hbs',
+}));
+app.set("view engine", "hbs");
+
+// Login Middleware
+const login = (req, res, next) => {
+  if (!req.session.userId) {
+    res.redirect('/login');
+  } else {
+    next();
+  }
+}
+
+// DB
+const users = JSON.parse(fs.readFileSync('db.json'));
+
+// Routes
+
+app.get('/home', login, (req, res)=> {
+  res.send('Home');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', (req, res) => {
+  if(!req.body.email || !req.body.password) {
+    return res.status(400).send('Fill all the fields');
+  }
+  const user = users.find(user => user.email === req.body.email);
+  if (!user || user.password !== req.body.password) {
+    return res.status(400).send('Invalid Credentials');
+  }
+  req.session.userId = user.id;
+  res.redirect('/home');
+});
+
+app.get('/logout', login, (req, res) => {
+  req.session.destroy();
+  res.send('Logged Out');
+});
+
+app.get('/edit', login, (req, res) => {
+  res.render('edit');
+});
+
+app.post('/edit', login, (req, res) => {
+  const user = users.find(user => user.id === req.session.userId);
+  user.email = req.body.email;
+  console.log(`User ${user.id} email changed to ${user.email}`);
+  res.send('Email Changed');
+});
+
+// Server
+
+app.listen(PORT, () => console.log('Listening on port ', PORT));
